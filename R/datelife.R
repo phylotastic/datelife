5c939ab57f673962f2f320228ab50273adbe978c
Brian OMeara <omeara.brian@gmail.com>
2012-06-05 20:27:37 -0400

    example code

---
+++ R/datelife.R
@@ -0,0 +1,50 @@
+#skeleton of code to make open access info on dating life
+library(ape)
+library(phylobase)
+
+ReadRDFTree <- function(identifier, format = "phylo") {
+	# reads a tree in RDF format from the hypothetical data store
+	tree<-read.file( identifier ) #do magic
+	if (format == "phylo") {
+		return(as.phylo(tree)) #assumes we have a converter
+	}
+}
+
+#in case we want to cache. Not clear we do
+ComputePatristicDistance <- function(phy, test=TRUE) {
+	# stores the distance between taxa
+	if (test) {
+		if (!is.ultrametric(phy)) {
+			stop("currently we require that chronograms be ultrametric") # can pad them so that terminals all reach to present
+		}
+	}
+	patristic.matrix<-cophenetic(phy)
+	return(patristic.matrix)
+}
+
+GetSubsetMatrix <- function(patristic.matrix, taxa, phy4=NULL) {
+  #gets a subset of the patristic.matrix. If you give it a phylo4 object, it can check to see if taxa are a clade
+  patristic.matrix <- patristic.matrix[ rownames(patristic.matrix) %in% taxa,colnames(patristic.matrix) %in% taxa ]
+  problem <- "none"
+  final.size <- sum(rownames(patristic.matrix) %in% taxa) # returns number of matches
+  if (final.size < length(taxa)) {
+    problem <- "missing some taxa on chronogram, so this is probably an underestimate" # fewer taxa on final matrix than we asked for
+    if (final.size < 2 ) {
+      problem <- "insufficient coverage" # we either have one species or zero. Not enough for an MRCA 
+      patristic.matrix <- NA # to make sure no one uses the zero by mistake
+    }
+  }
+  if(!is.null(phy4)) {
+    if (length(descendants(phy4, MRCA(phy4, taxa), type="tips")) > taxa) {
+       problem <- "set of taxa not a clade, so this is probably an overestimate"
+    }
+  }
+  return(list(patristic.matrix=patristic.matrix,problem=problem))
+}
+
+GetAge <- function(patristic.matrix) {
+  # 0.5 since patristic distance is down to the root and back up
+  return(0.5 * max(patristic.matrix)) 
+}
+
+
---
+++ R/example_run_1.R
@@ -0,0 +1,29 @@
+library(PhyloOrchard) #install.packages("PhyloOrchard", repos="http://R-Forge.R-project.org")
+library(phylobase)
+data(BinindaEmondsEtAl2007)
+best.phylo4<-BinindaEmondsEtAl2007$trees$best
+best.phylo3<-reorder(as(best.phylo4,"phylo"))
+
+patristic.distance <- ComputePatristicDistance(best.phylo3)
+
+# this should work
+mrca.age <- GetAge(GetSubsetMatrix( patristic.distance, c("Homo_sapiens","Pan_paniscus"))$patristic.matrix)
+print(mrca.age)
+
+# so will this, with three taxa
+mrca.age <- GetAge(GetSubsetMatrix( patristic.distance, c("Homo_sapiens", "Pan_paniscus", "Mus_musculus"))$patristic.matrix)
+print(mrca.age)
+
+# if we want all the pairwise dates, just print the output matrix, halved (patristic distance = twice the depth)
+print(GetSubsetMatrix( patristic.distance, c("Homo_sapiens", "Pan_paniscus", "Mus_musculus"))$patristic.matrix/2)
+
+# now try to find a group with a missing species:
+result<-GetSubsetMatrix( patristic.distance, c("Homo_sapiens", "Pan_paniscus","Unicorn"))
+print(result$problem)
+print(GetAge(result$patristic.matrix))
+
+# now have insufficient overlap even for an estimate
+result<-GetSubsetMatrix( patristic.distance, c("Homo_sapiens", "Griffin","Unicorn"))
+print(result$problem)
+print(GetAge(result$patristic.matrix))
+
