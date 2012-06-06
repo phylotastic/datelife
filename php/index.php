4fe13dec6ad376bffe135830b3be58576eb611d2
Brian O'Meara <omeara.brian@gmail.com>
2012-06-06 08:49:49 -0400

    working website

--- php/index.php
+++ php/index.php
@@ -3,6 +3,11 @@
   <title>DateLife</title>
  </head>
  <body>
- <?php echo "<p>It's Phylotastic!</p>"; ?> 
+<p><a href="http://www.evoio.org/wiki/Phylotastic"><img src="http://www.evoio.org/wg/evoio/images/f/f1/Phylotastic_logo.png"></a></p>
+<p>Demo after a day of coding: enter a string of mammal names. It will give you the age of their most recent common ancestor from the Bininda-Emonds et al. mammal tree.</p>
+<p>Use the syntax <code>Genus_species,Genus_species</code>: comma-delimited, no spaces, underscores in names</p>
+ <p><form action = "http://datelife.org/cgi-bin/R/test" method="get">
+ <p>Taxa: <input type="text" name="taxa" value="Homo_sapiens,Pan_paniscus,Mus_musculus" size="80"> <input type="submit" value="Send"></p>
+</form></p>
  </body>
 </html>
