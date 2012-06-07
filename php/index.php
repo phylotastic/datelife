b05a252c3cdc59f98f4c1f9ad4fe0e861a1744a1
Brian OMeara <omeara.brian@gmail.com>
2012-06-07 12:38:30 -0400

    output options

--- php/index.php
+++ php/index.php
@@ -7,7 +7,13 @@
 <p>Demo after a day of coding: enter a string of mammal names. It will give you the age of their most recent common ancestor from the Bininda-Emonds et al. mammal tree.</p>
 <p>Use the syntax <code>Genus_species,Genus_species</code>: comma-delimited, no spaces, underscores in names</p>
  <p><form action = "http://datelife.org/cgi-bin/R/test" method="get">
- <p>Taxa: <input type="text" name="taxa" value="Homo_sapiens,Pan_paniscus,Mus_musculus" size="80"> <input type="submit" value="Send"></p>
+ <p>Taxa: <input type="text" name="taxa" value="Homo_sapiens,Pan_paniscus,Mus_musculus" size="80"> 
+ <br />Return format: <select name="format">
+ <option value="html">HTML</option>
+ <option value="nexml">NeXML (in the future)</option>
+ <option value="rda">R data file (in the future)</option>
+ </select>
+ <input type="submit" value="Send"></p>
 </form></p>
 <p>Note source code is available at <a href="https://bitbucket.org/bomeara/datelife">https://bitbucket.org/bomeara/datelife</a></p>
 <p>We use the <a href="http://r-forge.r-project.org/projects/phyloorchard/">PhyloOrchard</a> R package to store trees; you can install it by doing<br />
