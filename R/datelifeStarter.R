3ff22d906332db9346c9da01a59b2394a33ed94e
Brian OMeara <omeara.brian@gmail.com>
2012-06-07 16:05:46 -0400

    I had trouble making a list properly. D'oh

--- R/datelifeStarter.R
+++ R/datelifeStarter.R
@@ -9,44 +9,44 @@ print(system("ls",intern=TRUE))
 print("beginning to load data")
 
 
-citations<-append(citations,"Heath et al. 2012")
-embargoed<-append(embargoed,FALSE)
+citations<-c(citations,"Heath et al. 2012")
+embargoed<-c(embargoed,FALSE)
 load("heath.rda")
 print(ls())
-studies<-append(studies,heath.all)
+studies[[length(studies)+1]]<-heath.all
 rm(heath.all)
 print(citations)
 
-citations<-append(citations,"10K trees Perissodactyla, V1")
-embargoed<-append(embargoed,FALSE)
+citations<-c(citations,"10K trees Perissodactyla, V1")
+embargoed<-c(embargoed,FALSE)
 load("harvard10k.perissodactyla.rda")
 print(ls())
-studies<-append(studies,harvard10k.perissodactyla.all)
+studies[[length(studies)+1]]<-harvard10k.perissodactyla.all
 rm(harvard10k.perissodactyla.all)
 print(citations)
 
 if (1>2) {
-citations<-append(citations,"Bininda-Emonds et al. 2007")
-embargoed<-append(embargoed,FALSE)
+citations<-c(citations,"Bininda-Emonds et al. 2007")
+embargoed<-c(embargoed,FALSE)
 load("bininda.emonds.rda")
 print(ls())
-studies<-append(studies,bininda.emonds.all)
+studies[[length(studies)+1]]<-studies,bininda.emonds.all
 rm(bininda.emonds.all)
 print(citations)
 
-citations<-append(citations,"10K trees Carnivores, V1")
-embargoed<-append(embargoed,FALSE)
+citations<-c(citations,"10K trees Carnivores, V1")
+embargoed<-c(embargoed,FALSE)
 load("harvard10k.carnivores.rda")
 print(ls())
-studies<-append(studies,harvard10k.carnivores.all)
+studies[[length(studies)+1]]<-harvard10k.carnivores.all
 rm(harvard10k.carnivores.all)
 print(citations)
 
-citations<-append(citations,"10K trees Primates, V3")
-embargoed<-append(embargoed,FALSE)
+citations<-c(citations,"10K trees Primates, V3")
+embargoed<-c(embargoed,FALSE)
 load("harvard10k.primates.rda")
 print(ls())
-studies<-append(studies,harvard10k.primates.all)
+studies[[length(studies)+1]]<-harvard10k.primates.all
 rm(harvard10k.primates.all)
 print(citations)
 }
