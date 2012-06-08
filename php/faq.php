f7684537396971fabb3d6904c7ebc69f30c4f3d4
Brian OMeara <omeara.brian@gmail.com>
2012-06-08 15:48:28 -0400

    start of faq

---
+++ php/faq.php
@@ -0,0 +1,10 @@
+<html>
+<?php include("pagestart.html"); ?>
+<h2>FAQ</h2>
+<p><b>Don't you know about <a href="http://www.timetree.org">TimeTree</a>? Why do a new site?</b></p>
+<p><a href="http://www.timetree.org"a>TimeTree</a> is a great resource. It has expert-derived dates as well as dates from published trees. It also has a good web interface (for mobile devices, too) and now free <a href="http://itunes.apple.com/us/app/timetree/id372842500?mt=8">iPhone</a> and <a href="http://itunes.apple.com/us/app/timetree-hd/id468820058?mt=8">iPad</a> interfaces. You can even get free book chapters and poster images or buy a physical book or poster. What you may not do is reuse the data in an automated manner (at least when we started our project in June 2012; hopefully this FAQ is now out of date on this point). For example, <a href="http://www.timetree.org"a>TimeTree</a> displays this note (as of June 8, 2012):
+<br /><center><img src="timetreereuse.png"></center>
+<br />and the FAQ states (as of June 8, 2012):
+<br /><center><img src="timetreedownload.png"></center>
+<br />Requests to allow download of the calibrated tree or other widespread reuse have been consistently but politely declined. Reusing existing work makes a great deal of sense, but since this was not possible, we decided to develop our own.</p>
+<?php include("pageend.html"); ?>
