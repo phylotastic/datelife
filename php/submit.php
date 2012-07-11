6819e44598f2400e17c8b67d8b8ff6c7dc9530e4
Brian O'Meara <omeara.brian@gmail.com>
2012-07-11 16:03:09 -0400

    submission

---
+++ php/submit.php
@@ -0,0 +1,5 @@
+<html>
+<?php include("pagestart.html"); ?>
+<h2>Submit a tree</h2>
+<p><iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dFV4cHlPSVBKLTlCRkVLUFZmYl9iSHc6MQ" width="760" height="1413" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe></p>
+<?php include("pageend.html"); ?>
