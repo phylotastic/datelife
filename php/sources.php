15f4d7919ed80bb3cbc77c8bbb9be66c434c8687
Brian O'Meara <omeara.brian@gmail.com>
2012-08-28 10:02:40 -0400

    sources

---
+++ php/sources.php
@@ -0,0 +1,7 @@
+<html>
+<?php include("pagestart.html"); ?>
+<h2>Sources</h2>
+<br />
+<p>These are the papers used in DateLife; they are stored in a Mendeley group for easy download of citations</p>
+<iframe src="http://www.mendeley.com/groups/2307701/datelife/widget/29/10/" frameborder="0" allowTransparency="true" style="width:600px;height:900px;"></iframe><p style="width:600px"><a href="http://www.mendeley.com/groups/2307701/datelife/" title="DateLife on Mendeley">DateLife</a> is a group in <a href="http://www.mendeley.com/groups/biological-sciences/" title="Biological Sciences on Mendeley">Biological Sciences</a> on <a href="http://www.mendeley.com/" title="Mendeley">Mendeley</a>.</p>
+<?php include("pageend.html"); ?>
