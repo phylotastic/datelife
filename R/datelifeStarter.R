cac2cfaa873a689e10e0d223c7b79a0edf54147a
KlausVigo <klaus.schliep@gmail.com>
2013-01-31 19:09:05 +0100

    load trees instead of matrices

--- R/datelifeStarter.R
+++ R/datelifeStarter.R
@@ -11,96 +11,98 @@ print("beginning to load data")
 
 citations<-c(citations,"Heath et al. 2012")
 embargoed<-c(embargoed,FALSE)
-load("heath.rda")
+data(HeathEtAl2012)
 print(ls())
-studies[[length(studies)+1]]<-heath.all
-rm(heath.all)
+studies[[length(studies)+1]]<-HeathEtAl2012_tree
+rm(HeathEtAl2012_tree)
 print(citations)
 
 citations<-c(citations,"10K trees Perissodactyla, V1")
 embargoed<-c(embargoed,FALSE)
-load("harvard10k.perissodactyla.rda")
+harvard10k.perissodactyla.trees<-read.nexus("TreeBlock_10kTrees_Perissodactyla_Version1.nex")
 print(ls())
-studies[[length(studies)+1]]<-harvard10k.perissodactyla.all
-rm(harvard10k.perissodactyla.all)
+studies[[length(studies)+1]]<-harvard10k.perissodactyla.trees
+rm(harvard10k.perissodactyla.trees)
 print(citations)
 
 citations<-c(citations,"Bininda-Emonds et al. 2007")
 embargoed<-c(embargoed,FALSE)
-load("bininda.emonds.rda")
+data(BinindaEmondsEtAl2007)
 print(ls())
-studies[[length(studies)+1]]<-bininda.emonds.all
-rm(bininda.emonds.all)
+studies[[length(studies)+1]]<-BinindaEmondsEtAl2007
+rm(BinindaEmondsEtAl2007)
 print(citations)
 
 citations<-c(citations,"10K trees Carnivores, V1")
 embargoed<-c(embargoed,FALSE)
-load("harvard10k.carnivores.rda")
+harvard10k.carnivores.trees<-read.nexus("TreeBlock_10kTrees_Carnivora_Version1.nex")
 print(ls())
-studies[[length(studies)+1]]<-harvard10k.carnivores.all
-rm(harvard10k.carnivores.all)
+studies[[length(studies)+1]]<-harvard10k.carnivores.trees
+rm(harvard10k.carnivores.trees)
 print(citations)
 
 citations<-c(citations,"10K trees Primates, V3")
 embargoed<-c(embargoed,FALSE)
-load("harvard10k.primates.rda")
+harvard10k.primates.trees<-read.nexus("TreeBlock_10kTrees_Primates_Version3.nex")
 print(ls())
-studies[[length(studies)+1]]<-harvard10k.primates.all
-rm(harvard10k.primates.all)
+studies[[length(studies)+1]]<-harvard10k.primates.trees
+rm(harvard10k.primates.trees)
 print(citations)
 
 citations<-c(citations,"Alfaro et al. 2009")
 embargoed<-c(embargoed,FALSE)
-load("alfaro2009.rda")
+data(AlfaroEtAl2009)
 print(ls())
-studies[[length(studies)+1]]<-alfaro2009.all
-rm(alfaro2009.all)
+studies[[length(studies)+1]]<-AlfaroEtAl2009_tree
+rm(AlfaroEtAl2009_tree)
 print(citations)
 
 citations<-c(citations,"Bergmann and Irschick 2012")
 embargoed<-c(embargoed,FALSE)
-load("bergmann.rda")
+data(BergmannEtAl2012)
 print(ls())
-studies[[length(studies)+1]]<-bergmann.all
-rm(bergmann.all)
+studies[[length(studies)+1]]<-BergmannEtAl2012
+rm(BergmannEtAl2012)
 print(citations)
 
+# Eastman et al. unpublished tree of salamanders
 citations<-c(citations,"embargoed")
 embargoed<-c(embargoed,TRUE)
-load("embargoed2.rda")
+data(Unpub1)
 print(ls())
-studies[[length(studies)+1]]<-embargoed2.all
-rm(embargoed2.all)
+studies[[length(studies)+1]]<-Unpub1
+rm(Unpub1)
 print(citations)
 
 citations<-c(citations,"Oaks 2011")
 embargoed<-c(embargoed,FALSE)
-load("oaks2011.rda")
+data(Oaks2012)
 print(ls())
-studies[[length(studies)+1]]<-oaks2011.all
-rm(oaks2011.all)
+studies[[length(studies)+1]]<-Oaks2011
+rm(Oaks2011)
 print(citations)
 
 citations<-c(citations,"Jaffe et al. 2011")
 embargoed<-c(embargoed,FALSE)
-load("jaffe.rda")
+data(JaffeEtAl2011)
 print(ls())
-studies[[length(studies)+1]]<-jaffe.all
-rm(jaffe.all)
+studies[[length(studies)+1]]<-JaffeEtAl2011
+rm(JaffeEtAl2011)
 print(citations)
 
 citations<-c(citations,"Zhang and Wake 2009")
 embargoed<-c(embargoed,FALSE)
-load("zhangwake2009.rda")
+data(ZhangandWake2009)
 print(ls())
-studies[[length(studies)+1]]<-zhangwake2009.all
-rm(zhangwake2009.all)
+studies[[length(studies)+1]]<-ZhangandWake2009
 print(citations)
 
 print(paste("number of studies = ",length(studies)))
 
 print(paste("number of species = ",length(unique(unlist(sapply(studies,rownames))))))
 
+function(ifelse(class(x)=="multiPhylo", length(x), 1L)
 print(paste("number of trees total = ",sum(sapply(studies,dim)[3,])))
 
 setwd(original.wd)
+
