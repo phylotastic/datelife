05ac1ae14fa4c7be0579bc92cc2d06fea8216490
schamberlain <myrmecocystus@gmail.com>
2013-01-31 16:40:34 -0700

    removed cleaning_tests.r and moved its function to cleaning.r, minor changes to fxn's
    added more documentation for functions

--- R/cleaning.r
+++ R/cleaning.r
@@ -3,7 +3,7 @@
 #' Find only rows in a data.frame where the values don't match in a row
 #' 
 #' @import plyr
-#' @param x A data.frame
+#' @param df A data.frame
 #' @examples
 #' dff <- data.frame(FabreEtAl2009$tip.label, out$tip.label)
 #' nonmatching_by_row(dff)
@@ -87,9 +87,17 @@ detect_unid <- function(taxon){
 #' 
 #' @import taxize
 #' @param phylo Phylogeny object, class phylo
+#' @param charvector Character vector.
 #' @param source_ Source to match names to. One of ncbi, iPlant
 #' @param splitby Length of chunks by which to split species list
+#' @param writefile Write file to directory or not. Remember to change directory to 
+#' 		write to in the function. Right now it is set to 
+#' 		"/Library/WebServer/Sites/datelife.org/datelife/data/"
+#' @param byfilename Defaults to TRUE - if TRUE, then interprets the phylo argument input 
+#' 		using eval() so executes it (used in name fixing on stored trees). If FALSE, 
+#' 		then you can specify phylo as a phylo object instead of the name of that object.
 #' @examples \dontrun{
+#' # Cleaning stored trees
 #' # A phylogeny as input
 #' library(doMC)
 #' out <- checknames(phylo="BergmannEtAl2012", source_="NCBI", splitby=200)
@@ -108,9 +116,13 @@ detect_unid <- function(taxon){
 #' out <- checknames(phylo="Pyron2011", source_="NCBI")
 #' out <- checknames(phylo="SantiniEtAl2009", source_="NCBI", splitby=50)
 #' 
+#' # Cleaning user input trees
 #' # A character vector as input (of species names)
-#' out <- checknames(charvector=AlfaroEtAl2009_tree$tip.label, source_="NCBI", splitby=50)
-#' out <- checknames(charvector=AlfaroEtAl2009_tree$tip.label[1:20], source_="NCBI")
+#' out <- checknames(charvector=AlfaroEtAl2009$tip.label[1:10], source_="NCBI")
+#' 
+#' # A phylo object - user suppressMessages() around the fxn
+#' mmm<-drop.tip(AlfaroEtAl2009, 1:190)
+#' out <- suppressMessages(checknames(phylo=mmm, source_="NCBI", byfilename=FALSE))
 #' }
 checknames <- function(phylo=NULL, charvector=NULL, source_ = "NCBI", 
 	splitby = NULL, writefile = FALSE, byfilename = TRUE)
@@ -249,7 +261,55 @@ checknames <- function(phylo=NULL, charvector=NULL, source_ = "NCBI",
 			{ return(temppp) }
 		} else
 		{
-			charvector <- tips_2
-			return(charvector)
+			return(tips_2)
 		}
+}
+
+
+
+#' Test timing of name cleaning on datelife trees
+#' 
+#' @import plyr ggplot2 taxize stringr
+#' @param listoftrees List of trees, each tree in phylo format, not multiPhylo
+#' 		format. 
+#' @param plotresults Plot results or not.
+#' @examples \dontrun{
+#' treefilenames <- dir("/Users/scottmac2/phyloorchard/pkg/data")
+#' l_ply(treefilenames, function(x) load(paste("/Users/scottmac2/phyloorchard/pkg/data/",x,sep=""), .GlobalEnv))
+#' trees <- sapply(treefilenames, function(x) str_replace(x, ".rda", ""), USE.NAMES=F)
+#' binemonds <- BinindaEmondsEtAl2007[[1]]
+#' makesmallsubtrees <- function(phylo){
+#' 		drops <- c(2000,2500,3000,3500,4000,4100,4200,4300,4350,4400,4420,4450,4470,4500,4505)
+#' 		llply(rev(drops), function(x) drop.tip(phylo, tip=1:x))
+#' }
+#' mytreelist <- makesmallsubtrees(phylo = binemonds)
+#' library(ggplot2); library(plyr); library(lubridate)
+#' out <- time_cleaning(listoftrees = mytreelist, plotresults=TRUE)
+#' out[[1]] # data.frame
+#' out[[2]] # plot
+#' }
+time_cleaning <- function(listoftrees, plotresults=FALSE){
+	checknames_safe <- plyr::failwith(NULL, checknames)
+	temp <- llply(listoftrees, function(tree){
+		start <- Sys.time()
+		result <- suppressMessages(checknames_safe(phylo=tree, source_="MSW3", splitby=100, byfilename=FALSE))
+		stop_ <- Sys.time()
+		elapsed <- as.duration(stop_-start)
+		as.numeric(elapsed)
+	}, .progress="text"
+	)
+	
+	names(temp) <- sapply(listoftrees, Ntip, USE.NAMES=F)
+	resultsout <- ldply(temp)
+	names(resultsout) <- c("num_tips","elapsed")
+	resultsout$num_tips <- as.numeric(resultsout$num_tips)
+	
+	if(plotresults){
+		p <- ggplot(resultsout, aes(num_tips, elapsed)) + 
+			theme_bw(base_size=18) +
+			geom_point(size = 4) + 
+			labs(y = "Elapsed time", x = "Number of tips")
+		list(resultsout, p)
+	} else
+	{ resultsout }
 }
