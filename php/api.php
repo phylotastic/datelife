bc8f95ae0581a221e25f2a019686f81211431509
Brian OMeara <omeara.brian@gmail.com>
2013-01-31 11:16:32 -0500

    api done

--- php/api.php
+++ php/api.php
@@ -11,4 +11,15 @@
 <br /><b>bestguess</b>: A single floating point number with the median age of the MRCA across studies
 <br /><b>bestguessuncert</b>: Comma-delimited: median age, min age, max age
 </p>
+<p><b>partial=</b>
+<br /><b>liberal</b>: Return info from trees that have any taxon overlap with your query (even though this may result in an age underestimate)
+<br /><b>conservative</b>: Return info from trees that have complete taxon overlap with your query (even though this may be few, or zero, trees)
+</p>
+<p><b>useembargoed=</b>
+<br /><b>yes</b>: Yes, use all data in the database (even info from as yet unpublished trees by DateLife contributors)
+<br /><b>no</b>: No, use only peer-reviewed, published results (almost all of the dataset, except for a few big trees)
+</p>
+<p><b>uncertainty=</b>
+<br /><b><i>0-100</i></b>: You can enter any number from 0 to 100 here. If there is a single point estimate, how uncertain should the age estimate be interpreted as (60=age of MRCA +/- 60% of this age)
+</p>
 <?php include("pageend.html"); ?>
