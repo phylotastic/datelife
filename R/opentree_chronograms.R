62ca9c431ca2c3a1db0cfca1c9dd4e2b96be593b
Brian O'Meara <omeara.brian@gmail.com>
2015-08-09 02:32:09 -0400

    wrapped in try statement

--- R/opentree_chronograms.R
+++ R/opentree_chronograms.R
@@ -16,7 +16,8 @@ for (chrono.index in sequence(length(chronogram.matches))) {
 	print(paste("tree number", chrono.index, "of", length(chronogram.matches)))
 	study.id <- unlist(unname(chronogram.matches[[chrono.index]][1]))
 	tree.id <- unname(unlist(chronogram.matches[[chrono.index]][[2]][[1]][2]))
-	new.tree <- get_study_tree(study_id=study.id, tree_id=tree.id, tip_label='ott_taxon_name')
+	new.tree <- NULL
+	try(new.tree <- get_study_tree(study_id=study.id, tree_id=tree.id, tip_label='ott_taxon_name'))
 	print(new.tree)
 	if(HasBrlen(new.tree)) {
 		trees[[chrono.index]] <-new.tree
