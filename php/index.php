a4b168186cac3e140b72777717a8f7fab3ede10b
Brian O'Meara <omeara.brian@gmail.com>
2015-08-09 00:28:22 -0400

    added short blurb about NSF, OToL, rOpenSci, and TreeBase

--- php/index.php
+++ php/index.php
@@ -1,6 +1,6 @@
 <html>
 <?php include("pagestart.html"); ?>
-<p>This will give the age of the most recent common ancestor of a set of taxa, or stretch an input tree, based on available time-calibrated trees. It is still actively in development, so it may break at times. Currently it mostly has mammals in the database, but we are actively adding taxa. See the <a href="http://datelife.org/faq.php">FAQ</a> page for more info on this project, and the <a href="http://phylotastic.org">Phylotastic</a> page for more info on the overall hackathon, of which this was just one outcome.</p>
+<p>This will give the age of the most recent common ancestor of a set of taxa, or stretch an input tree, based on available time-calibrated trees. It is still actively in development, so it may break at times. Currently it mostly has mammals in the database, but we are actively adding taxa. See the <a href="http://datelife.org/faq.php">FAQ</a> page for more info on this project, and the <a href="http://phylotastic.org">Phylotastic</a> page for more info. This was started at a NESCent hackathon, added to at others, and eventually NSF-funded as part of Phylotastic.</p>
  <p><form action = "http://datelife.org/cgi-bin/R/result" method="get">
  <p>Use the syntax <code>Genus_species,Genus_species</code>: comma-delimited, no spaces, underscores in names, i.e., <br /><b>Rhinoceros_unicornis,Equus_caballus,Mus_musculus</b><br />for a list of species in the taxa box, <b>OR</b> a Newick tree string, ending with a semicolon: <br /><b>((Rhinoceros_unicornis,Equus_caballus),Mus_musculus);</b><br />The tree can have branch lengths. Note that stretching a tree is much slower than just getting ages for a list of taxa. It uses a method by <a href="http://onlinelibrary.wiley.com/doi/10.1111/2041-210X.12051/abstract">Eastman et al. 2013</a></p>
  <p>Input: <input type="text" name="input" value="" size="80"></p>
@@ -49,5 +49,7 @@
 <p>Note source code is available at <a href="https://bitbucket.org/bomeara/datelife">https://bitbucket.org/bomeara/datelife</a></p>
 <p>We use the <a href="http://r-forge.r-project.org/projects/phyloorchard/">PhyloOrchard</a> R package to store trees; you can install it by doing<br />
 <code>install.packages("PhyloOrchard", repos="http://R-Forge.R-project.org")</code></p>
+<br />
+<p>DateLife is part of the Phylotastic project, funded by NSF. It uses resources from the Open Tree of Life project, rOpenSci, and TreeBase. To help, please <a href="https://tree.opentreeoflife.org/curator">add trees</a> to OpenTree's database! [they can be yours or others']</p>
 
 <?php include("pageend.html"); ?>
