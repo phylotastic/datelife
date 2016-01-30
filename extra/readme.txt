ec6acdb091cde8e72e88885a67f1b54138337951
Brian O'Meara <omeara.brian@gmail.com>
2016-01-29 22:34:43 -0500

    moving web code to datelife-web

--- extra/readme.txt
+++
@@ -1,3 +0,0 @@
-This holds a zipped function by Klaus Schliep to quickly prune trees. It will eventually be in phangorn. To install it, do
-
-sudo R CMD INSTALL --byte-compile datelife2_0.1.tar.gz
--- php/sources.php
+++
@@ -1,7 +0,0 @@
-<html>
-<?php include("pagestart.html"); ?>
-<h2>Sources</h2>
-<br />
-<p>Here are the most recently added sources of trees to DateLife; the full set is stored in a Mendeley group for easy download of citations</p>
-<iframe src="http://www.mendeley.com/groups/2307701/datelife/widget/29/10/" frameborder="0" allowTransparency="true" style="width:600px;height:900px;"></iframe><p style="width:600px"><a href="http://www.mendeley.com/groups/2307701/datelife/" title="DateLife on Mendeley">DateLife</a> is a group in <a href="http://www.mendeley.com/groups/biological-sciences/" title="Biological Sciences on Mendeley">Biological Sciences</a> on <a href="http://www.mendeley.com/" title="Mendeley">Mendeley</a>.</p>
-<?php include("pageend.html"); ?>
