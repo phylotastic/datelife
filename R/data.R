97c6ecd743730007d83190ec687c5cff52e2cd08
Brian O'Meara <omeara.brian@gmail.com>
2016-04-06 23:40:35 -0400

    documentation for datelife.cache

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Cached chronograms and other data from OpenTree
+#'
+#' A list with four elements for >100 chronograms from OpenTree
+#' 
+#' @format A list with four lists contained within it
+#' \describe{
+#'   \item{authors} List of lists of authors for the included studies
+#'   \item{curators} List of lists of curators for the included studies
+#'   \item{studies} List of study identifiers
+#'   \item{trees} List storing the chronograms from OpenTree
+#' }
+#' @source \url{http://opentreeoflife.org}
+"datelife.cache"
