5e3e5e75ab4a630748c3c3dcf69d80d54d7cd0b1
Brian O'Meara <omeara.brian@gmail.com>
2016-04-14 21:26:05 -0400

    changing run syntax to work with datelifeweb

--- R/result.R
+++ R/result.R
@@ -7,13 +7,16 @@
 #' @param usetnrs Whether to use OpenTree's TNRS for the input
 #' @param approximatematch If using TNRS, use approximate matching
 #' @param datelife.cache The list of lists containing the input trees and other info
-#' @return Nothing
+#' @return results in the desired format
 #' @export
-run<-function(input=c("Rhea americana", "Pterocnemia pennata", "Struthio camelus"), format="html", partial="yes", plot.width=600, plot.height=600, usetnrs="no", approximatematch="yes", datelife.cache=datelife.cache) {
+run<-function(input=c("Rhea americana", "Pterocnemia pennata", "Struthio camelus"), format="html", partial="yes", plot.width=600, plot.height=600, usetnrs="no", approximatematch="yes", datelife.cache=NULL) {
+	if(is.null(datelife.cache)) {
+	  data(opentree_chronograms)
+	  }
   #convert from HTML input to Boolean
   partial <- ifelse(partial=="yes", TRUE, FALSE)
   usetnrs <- ifelse(usetnrs=="yes", TRUE, FALSE)
   approximatematch <- ifelse(approximatematch=="yes", TRUE, FALSE)
   filtered.results <- GetFilteredResults(input, partial, usetnrs, approximatematch, datelife.cache)
-  FastRWeb::out(SummarizeResults(filtered.results, output.format=format, partial, datelife.cache, suppress.citations=TRUE)  )
+  SummarizeResults(filtered.results, output.format=format, partial, datelife.cache, suppress.citations=TRUE) 
 }
