0ee54044c98c69ca2261e0858476115983b2efe0
Luna Luisa Sanchez Reyes <sanchez.reyes.luna@gmail.com>
2018-05-07 13:22:34 -0400

    install-dependencies.sh working unzip

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,6 +5,6 @@
 # get the package
 wget https://www2.math.su.se/PATHd8/PATHd8.zip -P /tmp
 # unzip it
-unzip tmp/PATHd8.zip -d usr/local/bin/
+unzip /tmp/PATHd8 -d /usr/local/bin/
 # compile
 cc PATHd8.c -O3 -lm -o PATHd8
