58083384a26e505e99086cad117b2df1f4fcae31
Brian O'Meara <omeara.brian@gmail.com>
2018-08-25 15:38:44 -0400

    adding functions for assessing contributions

--- R/cacheauthors.R
+++ R/cacheauthors.R
@@ -111,3 +111,106 @@ make_overlap_table <- function(results_table) {
   final.table <- final.table[,-1]
   return(final.table)
 }
+
+#' Associate TreeBase authors with studies
+#' @return data.frame with author last name, author first and other names, and comma delimited URLs for TreeBase studies
+#'
+make_treebase_associations <- function() {
+  all.studies <- treebase::download_metadata("", by="all")
+  unlist(all.studies[[1]])[which(names(unlist(all.studies[[1]])) == "creator")]
+  author.results <- data.frame()
+  for (i in sequence(length(all.studies))) {
+    authors <- study.id <- NULL
+    try(authors <- unlist(all.studies[[i]])[which(names( unlist(all.studies[[i]])) == "creator")])
+    try(study.id <- unlist(all.studies[[i]])["identifier"])
+    try(study.url <- gsub('purl.org/phylo/treebase/phylows/study/TB2:S', 'https://treebase.org/treebase-web/search/study/summary.html?id=', unlist(all.studies[[i]])["identifier"]))
+
+    try(publisher <- unlist(all.studies[[i]])["publisher"])
+    try(year <- unlist(all.studies[[i]])["date"])
+
+    if(!is.null(authors)) {
+      author.results <- rbind(author.results, expand.grid(person = authors, url = paste0('<a href="',study.url, '">',year, " ", publisher," TreeBase</a>"), stringsAsFactors = FALSE))
+    }
+    if(verbose) {
+      message(paste(i, "of", length(all.studies), "done."))
+    }
+  }
+
+  author.lumped <- aggregate(author.results$url, by=list(author=author.results$person), FUN=paste, collapse=", ")
+  colnames(author.lumped) <- c("person", "urls")
+  return(author.lumped)
+}
+
+#' Associate OToL authors with studies
+#' @return data.frame with author last name, author first and other names, and comma delimited URLs for OToL studies
+make_otol_associations <- function() {
+  #.res <- system("curl -X POST https://api.opentreeoflife.org/v3/studies/find_studies -H 'content-type:application/json'", intern=TRUE)
+  #.res <- httr::POST("https://api.opentreeoflife.org/v3/studies/find_studies")
+#  res <- vapply(.res[["matched_studies"]], function(x) x[["ot:studyId"]],character(1))
+
+  all.trees <- rbind(rotl::studies_find_trees(property = "is_deprecated", value = "false", verbose = TRUE, detailed = TRUE), rotl::studies_find_trees(property = "is_deprecated", value = "true", verbose = TRUE, detailed = TRUE))
+	trees <- list()
+	authors <- list()
+	curators <- list()
+	studies <- list()
+	dois <- list()
+  years <- list()
+	tree.count <- 0
+	bad.ones <- c()
+  studyids <- list()
+	# utils::opentree_chronograms
+
+	for (study.index in sequence(dim(all.trees)[1])) {
+		study.id <- all.trees$study_ids[study.index]
+		doi <- NULL
+		try(doi <- gsub('https?://(dx\\.)?doi.org/', '', attr(rotl::get_publication(rotl::get_study_meta(study.id)), "DOI")))
+		authors <- append(authors, NA)
+    dois <- append(dois, NA)
+    years <- append(years, NA)
+    studyids <- append(studyids, NA)
+    try(studyids[length(studyids)] <- study.id)
+    try(years[length(years)] <- rotl::get_study_meta(study.id)$nexml$`^ot:studyYear`)
+		if(length(doi) == 0){
+			warning(paste(study.id, "has no DOI attribute, author names will not be retrieved."))
+		} else {
+			try(authors[length(authors)] <- list(paste(as.character(knitcitations::bib_metadata(doi)$author))))
+      try(dois[length(dois)] <- doi)
+		}
+		try(studies <- append(studies, study.id))
+	}
+	for(i in sequence(length(authors))){
+		if(!any(is.na(authors[[i]])) & length(authors[[i]]) > 0){
+			Encoding(authors[[i]]) <- "latin1"
+			authors[[i]] <- iconv(authors[[i]], "latin1", "UTF-8")
+		}
+	}
+  studies <- unlist(studies)
+  urls <- paste0("<a href='https://tree.opentreeoflife.org/curator/study/view/", studies, "'>",years[[i]], " ",studyids[[i]], "OToL</a>")
+  combined.df <- data.frame()
+  invert_names <- function(x) {
+    x.split <- strsplit(x, " ")[[1]]
+    final.names <- paste0(x.split[length(x.split)], ", ", paste0(x.split[-length(x.split)], collapse=" "))
+    return(final.names)
+  }
+  for (i in sequence(length(authors))) {
+    for (author.index in sequence(length(authors[[i]]))) {
+      combined.df <- rbind(combined.df, data.frame(person=authors[[i]][author.index], url=urls[i], stringsAsFactors=FALSE))
+    }
+  }
+  author.lumped <- aggregate(combined.df$url, by=list(author=combined.df$person), FUN=paste, collapse=", ")
+  colnames(author.lumped) <- c("person", "urls")
+  return(author.lumped)
+}
+
+#' Find all authors and where they have deposited their trees
+#' @return a data.frame of person and urls
+#' @export
+make_all_associations <- function() {
+  tb <- make_treebase_associations()
+  ot <- make_otol_associations()
+  combined.df <- rbind(tb, ot)
+  author.lumped <- aggregate(combined.df$urls, by=list(author=combined.df$person), FUN=paste, collapse=", ")
+  colnames(author.lumped) <- c("person", "urls")
+  author.lumped <- author.lumped[order(author.lumped$person), ]
+  return(author.lumped)
+}
