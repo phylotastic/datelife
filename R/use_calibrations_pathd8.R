5f0a0aaba7dfd8220cff88dd0b098c29941c2846
Luna Luisa <sanchez.reyes.luna@gmail.com>
2019-05-15 17:46:55 -0400

    fix use calibrations pathd8

--- R/use_calibrations_pathd8.R
+++ R/use_calibrations_pathd8.R
@@ -9,8 +9,22 @@
 #' If that fails (often due to conflict between calibrations), it will expand the
 #' range of the minimum age and maximum age and try again. And repeat.
 #' If expand = 0, it uses the summarized calibrations.
-# phy <- tax_phyloall_bold[[1]][[2]]
-# calibrations <- tax_othercalall[[1]][[2]]
+#' In some cases, it returns edge lengths in relative time (with maximum tree depth = 1)
+#' instead of absolute time, as given by calibrations. In this case, the function returns NA. 
+#' This is an issue from PATHd8.
+# i=3
+# phy <- tax_phyloall_bold[[1]][[i]]
+# calibrations <- tax_othercalall[[1]][[i]]
+# tax_phyloall_bold[[1]][[i]]$edge.length
+# tax_othercalall[[1]][[i]]$MaxAge
+# calibs <- match_all_calibrations(tax_phyloall_bold[[1]][[i]], tax_othercalall[[1]][[i]])
+# calibs$phy$edge.length
+# used_calibrations <- calibs$matched_calibrations
+# used_calibrations$MaxAge
+# chronogram <- geiger::PATHd8.phylo(calibs$phy, used_calibrations)
+# chronogram$edge.length
+# plot(chronogram, main = i)
+# ape::axisPhylo()
 use_calibrations_pathd8 <- function(phy, calibrations, expand = 0.1, giveup = 100){
     if (!inherits(phy, "phylo")){
 		message("phy is not a phylo object")
@@ -38,7 +52,7 @@ use_calibrations_pathd8 <- function(phy, calibrations, expand = 0.1, giveup = 10
         try(chronogram <- geiger::PATHd8.phylo(phy, used_calibrations), silent = TRUE)
         # original_calibrations <- get_all_calibrations(phy2)
         while(!inherits(chronogram, "phylo") & attempts < giveup) {
-            print(rep(attempts, 10))
+            # print(rep(attempts, 10))
             used_calibrations <- calibs$present_calibrations
             used_calibrations$MaxAge <- as.numeric(used_calibrations$MaxAge) * ((1+expand)^attempts)
             used_calibrations$MinAge <- used_calibrations$MinAge * ((1-expand)^attempts)
@@ -62,7 +76,7 @@ use_calibrations_pathd8 <- function(phy, calibrations, expand = 0.1, giveup = 10
             attempts <- attempts+1
         }
         if(attempts > 0 & inherits(chronogram, "phylo")) {
-            message("Dates are even more approximate than usual: had to expand constraints to have them agree")
+            message("Dates are even more approximate than usual: had to expand constraints to have them agree.")
         }
 
     } else { # alternative to expansion: summarize calibrations
@@ -76,7 +90,12 @@ use_calibrations_pathd8 <- function(phy, calibrations, expand = 0.1, giveup = 10
             "negative", fixing_method = 0, ultrametric = TRUE)
         # chronogram$edge.length[which(chronogram$edge.length<0)] <- 0
         if(is.null(chronogram$edge.length) | all(is.na(chronogram$edge.length))){
-            message("dating with PATHd8 was unsuccessful.")
+            message("PATHd8 returned a tree with no branch lengths.")
+            return(NA)
+        }
+        if(round(max(ape::node.depth.edgelength(chronogram)), digits = 3) == 1){
+            message("Edge lengths seem to be relative to maximum age = 1 (and not absolute to time given by calibrations).")
+            message("This is an issue from PATHd8; returning NA.")
             return(NA)
         }
         chronogram$dating_method <- "pathd8"
