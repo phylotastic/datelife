976b93b6672c517db17e8eb6e799a3ebf5b29a8c
Luna Sare <sanchez.reyes.luna@gmail.com>
2023-01-09 14:47:56 -0600

    files for precompiled vignettes

---
+++ vignettes/precompiled.R
@@ -0,0 +1,24 @@
+# to precompile vignettes
+
+#knit the .orig vignette file into Rmd
+# this generates usually two dirs at root, cache and figure
+knitr::knit("vignettes/making_bold_trees.Rmd.orig",
+            output = "vignettes/making_bold_trees.Rmd")
+
+# move figures to vignettes folder
+# needed for vignette compilation
+sapply(list.files("figure/"), function(x){
+  file.copy(from = paste0("figure/", x),
+            to = "vignettes",
+            overwrite = TRUE)
+})
+
+# cache is not needed for vignette compilation, just for rendering
+# sapply(list.files("cache/"), function(x){
+#   file.copy(from = paste0("cache/", x), to = "vignettes")
+# })
+
+# cache dir from root is ignored for build and git
+# figure dir is deleted
+# tip from http://theautomatic.net/2018/07/11/manipulate-files-r/
+unlink("figure", recursive = TRUE)
