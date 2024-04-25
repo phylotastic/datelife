11b9915801b0c243682e8f47c1b865dc1e6c8793
Luna Sare <sanchez.reyes.luna@gmail.com>
2024-04-24 22:26:08 -0700

    Add tests to empty tests.

--- tests/testthat/test_opentree_chronograms.R
+++ tests/testthat/test_opentree_chronograms.R
@@ -1,4 +1,14 @@
 test_that("get_opentree_chronograms function runs", {
   xx <- update_datelife_cache(write = TRUE, max_tree_count = 2) # runs in 2 minutes
-  # expect_true(inherits(xx$trees), "multiPhylo")
+  expect_true(inherits(xx$trees, "multiPhylo"))
+})
+
+test_that("opentree_chronograms object is ok", {
+  data("opentree_chronograms")
+  # ls(opentree_chronograms)
+  # opentree_chronograms$version
+  # opentree_chronograms$trees[[1]]
+  expect_true(
+    all(sapply(opentree_chronograms$trees, inherits, "phylo"))
+    )
 })
--- tests/testthat/test_queries.R
+++ tests/testthat/test_queries.R
@@ -1,5 +1,16 @@
 test_that("datelife queries work", {
-  make_datelife_query(input = "anatidae")
+  # does not retrieve all species:
+  xx <- make_datelife_query(input = "anatidae")
+  expect_true(inherits(xx, "datelifeQuery"))
+  expect_true(xx$cleaned_names == "Anatidae")
+  expect_true(is.numeric(xx$ott_ids))
+  expect_true(is.na(xx$phy))
   # get species of penguins
   xx <- make_datelife_query(input = "eudyptes", get_spp_from_taxon = TRUE)
+  expect_true(inherits(xx, "datelifeQuery"))
+  expect_true(is.character(xx$cleaned_names))
+  expect_true(length(xx$cleaned_names) > 1)
+  expect_true(is.numeric(xx$ott_ids))
+  expect_true(length(xx$ott_ids) > 1)
+  expect_true(is.na(xx$phy))
 })
